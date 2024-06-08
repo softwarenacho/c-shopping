76129ddc95dd1371b6794f7c977dce76fe17d1a4
Nacho Betancourt <trsgarp@gmail.com>
2024-06-07 23:57:30 -0600

    chore: language switcher initial state

--- components/LanguageSwitcher.js
+++ components/LanguageSwitcher.js
@@ -1,37 +1,45 @@
 import { useLanguageContext } from '@/context/LanguageContext'
 import { getDictionary } from '@/helpers/dictionaries'
 import { Icons } from 'components'
-import { useCallback, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 
 const LanguageSwitcher = () => {
   const translation = useLanguageContext()
-  const [lang, setLang] = useState('zh')
+  const [language, setLang] = useState('zh')
 
   const getDict = useCallback(
     async lng => {
       const lang = await getDictionary(lng || 'zh')
       translation?.setDict(lang)
+      setLang(lng)
     },
     [translation]
   )
 
   const changeLanguage = () => {
     if (typeof window !== 'undefined') {
-      const language = window.localStorage.getItem('lng')
-      const lng = language === 'zh' ? 'en' : 'zh'
+      const languageStorage = window.localStorage.getItem('lng')
+      const lng = languageStorage === 'zh' ? 'en' : 'zh'
       setLang(lng)
       window.localStorage.setItem('lng', lng)
       getDict(lng)
     }
   }
 
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      const languageStorage = window.localStorage.getItem('lng')
+      getDict(languageStorage)
+    }
+  }, [getDict])
+
   return (
     <div
       className="flex-center text-sm gap-x-2 cursor-pointer dropdown__button"
       onClick={() => changeLanguage()}
     >
-      {lang === 'en' && <button>En</button>}
-      {lang === 'zh' && <button>中文</button>}
+      {language === 'en' && <button>En</button>}
+      {language === 'zh' && <button>中文</button>}
       <Icons.Translate className="icon" />
     </div>
   )
